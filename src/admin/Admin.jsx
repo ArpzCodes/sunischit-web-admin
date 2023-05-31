db5f18ba129d1108ad66bb4f0bbbd6eba3083bc7
Sas4aZ <paudelsashwat16@gmail.com>
2023-05-31 16:46:48 +0545

    Home page

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import './Admin.css';
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Admin = () => {
-  return (
-    <div className="admin-panel">
-      <h2>Admin Panel</h2>
-      <ul>
-        <a href="regDriver"> <li>Register Drivers</li></a>
-        <a href="regUser"> <li>Register Users</li></a>
-        <a href="updtUser">  <li>Update Users</li></a>
-        <a href="updtDriver"><li>Update Drivers</li></a>
-        <a href="setPickup"><li>Set Pickup</li></a>
-       
-       
-      
-        
-      </ul>
-    </div>
-  );
+    return (
+        <>
+<Sidebar/>
+            <div className="admin-panel">
+
+                <h2 className="list-group">Admin Panel</h2>
+                <ul className="list-group">
+                    <a href="regDriver">
+                        <li className="list-group-item">Register Drivers</li>
+                    </a>
+                    <a href="regUser">
+                        <li className="list-group-item">Register Users</li>
+                    </a>
+                    <a href="updtUser">
+                        <li className="list-group-item">Update Users</li>
+                    </a>
+                    <a href="updtDriver">
+                        <li className="list-group-item">Update Drivers</li>
+                    </a>
+                    <a href="setPickup">
+                        <li className="list-group-item">Set Pickup</li>
+                    </a>
+
+
+                </ul>
+            </div>
+        </>
+    );
 };
 
 export default Admin;
