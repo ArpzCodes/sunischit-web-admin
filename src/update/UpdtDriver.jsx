c50852610abddb0a64f83cdaf276dca535a0b998
Sas4aZ <paudelsashwat16@gmail.com>
2023-06-02 23:14:26 +0545

    Update design done

--- src/update/UpdtDriver.jsx
+++ src/update/UpdtDriver.jsx
@@ -1,190 +1,214 @@
-import React, { useEffect, useState } from 'react';
-import { db, auth } from '../firebase/firebase';
+import React, {useEffect, useState} from 'react';
+import {db, auth} from '../firebase/firebase';
+import SidebarTest from "../components/sidebar/SidebarTest";
 
 const UpdtDriver = () => {
-  const [drivers, setDrivers] = useState([]);
-  const [updateDriver, setUpdateDriver] = useState(null);
-  const [updateEmail, setUpdateEmail] = useState('');
-  const [updateBusNo, setUpdateBusNo] = useState('');
-  const [updateFirstName, setUpdateFirstName] = useState('');
-  const [updateLastName, setUpdateLastName] = useState('');
-  const [updateNumber, setUpdateNumber] = useState('');
-  const [updateRoute, setUpdateRoute] = useState('');
+    const [drivers, setDrivers] = useState([]);
+    const [updateDriver, setUpdateDriver] = useState(null);
+    const [updateEmail, setUpdateEmail] = useState('');
+    const [updateBusNo, setUpdateBusNo] = useState('');
+    const [updateFirstName, setUpdateFirstName] = useState('');
+    const [updateLastName, setUpdateLastName] = useState('');
+    const [updateNumber, setUpdateNumber] = useState('');
+    const [updateRoute, setUpdateRoute] = useState('');
 
-  useEffect(() => {
+    useEffect(() => {
 
-    fetchDrivers();
-  }, []);
+        fetchDrivers();
+    }, []);
 
-  const fetchDrivers = async () => {
-    try {
-      const driversSnapshot = await db.collection('drivers').get();
-      const driversData = driversSnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setDrivers(driversData);
-    } catch (error) {
-      console.error('Error fetching drivers:', error);
-    }
-  };
+    const fetchDrivers = async () => {
+        try {
+            const driversSnapshot = await db.collection('drivers').get();
+            const driversData = driversSnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setDrivers(driversData);
+        } catch (error) {
+            console.error('Error fetching drivers:', error);
+        }
+    };
 
-  const handleUpdateDriver = async (driver) => {
-    setUpdateDriver(driver);
-    setUpdateEmail(driver.email);
-    setUpdateBusNo(driver.busNo);
-    setUpdateFirstName(driver.firstName);
-    setUpdateLastName(driver.lastName);
-    setUpdateNumber(driver.number);
-    setUpdateRoute(driver.route);
-  };
+    const handleUpdateDriver = async (driver) => {
+        setUpdateDriver(driver);
+        setUpdateEmail(driver.email);
+        setUpdateBusNo(driver.busNo);
+        setUpdateFirstName(driver.firstName);
+        setUpdateLastName(driver.lastName);
+        setUpdateNumber(driver.number);
+        setUpdateRoute(driver.route);
+    };
 
-  const handleSaveUpdate = async () => {
-    try {
-   
-      
-      await db.collection('drivers').doc(updateDriver.id).update({
-        email: updateEmail,
-        busNo: updateBusNo,
-        firstName: updateFirstName,
-        lastName: updateLastName,
-        number: updateNumber,
-        route: updateRoute,
-      });
+    const handleSaveUpdate = async () => {
+        try {
 
-      alert('Driver updated successfully!');
-      fetchDrivers();
-      setUpdateDriver(null);
-      clearUpdateForm();
-    } catch (error) {
-      console.error('Error updating driver:', error);
-    }
-  };
 
-  const handleDeleteDriver = async (driverId) => {
-    try {
-      await db.collection('drivers').doc(driverId).delete();
-      await auth.currentUser.delete();
+            await db.collection('drivers').doc(updateDriver.id).update({
+                email: updateEmail,
+                busNo: updateBusNo,
+                firstName: updateFirstName,
+                lastName: updateLastName,
+                number: updateNumber,
+                route: updateRoute,
+            });
 
-      alert('Driver deleted successfully!');
-      fetchDrivers();
-    } catch (error) {
-      console.error('Error deleting driver:', error);
-    }
-  };
+            alert('Driver updated successfully!');
+            fetchDrivers();
+            setUpdateDriver(null);
+            clearUpdateForm();
+        } catch (error) {
+            console.error('Error updating driver:', error);
+        }
+    };
 
-  const clearUpdateForm = () => {
-    setUpdateEmail('');
-    setUpdateBusNo('');
-    setUpdateFirstName('');
-    setUpdateLastName('');
-    setUpdateNumber('');
-    setUpdateRoute('');
-  };
+    const handleDeleteDriver = async (driverId) => {
+        try {
+            await db.collection('drivers').doc(driverId).delete();
+            await auth.currentUser.delete();
 
-  return (
-    <div>
-      <h2>Update Drivers</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Bus Number</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Number</th>
-            <th>Route</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {drivers.map((driver) => (
-            <tr key={driver.id}>
-             
-              <td>{driver.email}</td>
-              <td>{driver.busNo}</td>
-              <td>{driver.firstName}</td>
-              <td>{driver.lastName}</td>
-              <td>{driver.number}</td>
-              <td>{driver.route}</td>
-              <td>
-                <button onClick={() => handleUpdateDriver(driver)}>Edit</button>
-                <button onClick={() => handleDeleteDriver(driver.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            alert('Driver deleted successfully!');
+            fetchDrivers();
+        } catch (error) {
+            console.error('Error deleting driver:', error);
+        }
+    };
 
-      {updateDriver && (
-        <div>
-          <h3>Update Driver</h3>
-          <form>
-            <label>
-              Email:
-              <input
-                type="email"
-                value={updateEmail}
-                onChange={(e) => setUpdateEmail(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Bus Number:
-              <input
-                type="text"
-                value={updateBusNo}
-                onChange={(e) => setUpdateBusNo(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              First Name:
-              <input
-                type="text"
-                value={updateFirstName}
-                onChange={(e) => setUpdateFirstName(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Last Name:
-              <input
-                type="text"
-                value={updateLastName}
-                onChange={(e) => setUpdateLastName(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Number:
-              <input
-                type="text"
-                value={updateNumber}
-                onChange={(e) => setUpdateNumber(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Route:
-              <input
-                type="text"
-                value={updateRoute}
-                onChange={(e) => setUpdateRoute(e.target.value)}
-              />
-            </label>
-            <br />
-            <button type="button" onClick={handleSaveUpdate}>
-              Save
-            </button>
-            <button type="button" onClick={clearUpdateForm}>
-              Cancel
-            </button>
-          </form>
+    const clearUpdateForm = () => {
+        setUpdateEmail('');
+        setUpdateBusNo('');
+        setUpdateFirstName('');
+        setUpdateLastName('');
+        setUpdateNumber('');
+        setUpdateRoute('');
+    };
+
+    return (
+        <div className="d-flex">
+            <SidebarTest activePage="updtDriver"/>
+            <div className=" ps-4 pt-3">
+                <h2>Update Drivers</h2>
+                <table className="table  table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>Bus Number</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Number</th>
+                        <th>Route</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                    {drivers.map((driver) => (
+                        <tr key={driver.id}>
+
+                            <td>{driver.email}</td>
+                            <td>{driver.busNo}</td>
+                            <td>{driver.firstName}</td>
+                            <td>{driver.lastName}</td>
+                            <td>{driver.number}</td>
+                            <td>{driver.route}</td>
+                            <td>
+                                <div className="btn-group">
+                                    <button type="button" className="btn btn-outline-dark dropdown-toggle"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                        Action
+                                    </button>
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <button className="dropdown-item "
+                                                    onClick={() => handleUpdateDriver(driver)}>Edit
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        <li>
+                                            <button className="dropdown-item"
+                                                    onClick={() => handleDeleteDriver(driver.id)}>Delete
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+
+                {updateDriver && (
+                    <div>
+                        <h3>Update Driver</h3>
+                        <form>
+                            <label>
+                                Email:
+                                <input
+                                    type="email"
+                                    value={updateEmail}
+                                    onChange={(e) => setUpdateEmail(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Bus Number:
+                                <input
+                                    type="text"
+                                    value={updateBusNo}
+                                    onChange={(e) => setUpdateBusNo(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                First Name:
+                                <input
+                                    type="text"
+                                    value={updateFirstName}
+                                    onChange={(e) => setUpdateFirstName(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Last Name:
+                                <input
+                                    type="text"
+                                    value={updateLastName}
+                                    onChange={(e) => setUpdateLastName(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Number:
+                                <input
+                                    type="text"
+                                    value={updateNumber}
+                                    onChange={(e) => setUpdateNumber(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Route:
+                                <input
+                                    type="text"
+                                    value={updateRoute}
+                                    onChange={(e) => setUpdateRoute(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <button type="button" onClick={handleSaveUpdate}>
+                                Save
+                            </button>
+                            <button type="button" onClick={clearUpdateForm}>
+                                Cancel
+                            </button>
+                        </form>
+                    </div>
+                )}
+            </div>
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default UpdtDriver;
--- src/update/UpdtUser.jsx
+++ src/update/UpdtUser.jsx
@@ -1,189 +1,209 @@
-
-import React, { useEffect, useState } from 'react';
-import { db, auth } from '../firebase/firebase';
+import React, {useEffect, useState} from 'react';
+import {db, auth} from '../firebase/firebase';
+import SidebarTest from "../components/sidebar/SidebarTest";
 
 const UpdtUser = () => {
-  const [users, setUsers] = useState([]);
-  const [updateUser, setUpdateUser] = useState(null);
-  const [updateEmail, setUpdateEmail] = useState('');
-  const [updateFirstName, setUpdateFirstName] = useState('');
-  const [updateLastName, setUpdateLastName] = useState('');
-  const [updateNumber, setUpdateNumber] = useState('');
-  const [updatePickup, setUpdatePickup] = useState('');
-  const [updateRoute, setUpdateRoute] = useState('');
+    const [users, setUsers] = useState([]);
+    const [updateUser, setUpdateUser] = useState(null);
+    const [updateEmail, setUpdateEmail] = useState('');
+    const [updateFirstName, setUpdateFirstName] = useState('');
+    const [updateLastName, setUpdateLastName] = useState('');
+    const [updateNumber, setUpdateNumber] = useState('');
+    const [updatePickup, setUpdatePickup] = useState('');
+    const [updateRoute, setUpdateRoute] = useState('');
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-  const fetchUsers = async () => {
-    try {
-      const usersSnapshot = await db.collection('users').get();
-      const usersData = usersSnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setUsers(usersData);
-    } catch (error) {
-      console.error('Error fetching users:', error);
-    }
-  };
+    const fetchUsers = async () => {
+        try {
+            const usersSnapshot = await db.collection('users').get();
+            const usersData = usersSnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setUsers(usersData);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    };
 
-  const handleUpdateUser = async (user) => {
-    setUpdateUser(user);
-    setUpdateEmail(user.email);
-    setUpdateFirstName(user.firstName);
-    setUpdateLastName(user.lastName);
-    setUpdateNumber(user.number);
-    setUpdatePickup(user.pickup);
-    setUpdateRoute(user.route);
-  };
+    const handleUpdateUser = async (user) => {
+        setUpdateUser(user);
+        setUpdateEmail(user.email);
+        setUpdateFirstName(user.firstName);
+        setUpdateLastName(user.lastName);
+        setUpdateNumber(user.number);
+        setUpdatePickup(user.pickup);
+        setUpdateRoute(user.route);
+    };
 
-  const handleSaveUpdate = async () => {
-    try {
-      await db.collection('users').doc(updateUser.id).update({
-        email: updateEmail,
-        firstName: updateFirstName,
-        lastName: updateLastName,
-        number: updateNumber,
-        pickup: updatePickup,
-        route: updateRoute,
-      });
+    const handleSaveUpdate = async () => {
+        try {
+            await db.collection('users').doc(updateUser.id).update({
+                email: updateEmail,
+                firstName: updateFirstName,
+                lastName: updateLastName,
+                number: updateNumber,
+                pickup: updatePickup,
+                route: updateRoute,
+            });
 
-      alert('User updated successfully!');
-      fetchUsers();
-      setUpdateUser(null);
-      clearUpdateForm();
-    } catch (error) {
-      console.error('Error updating user:', error);
-    }
-  };
+            alert('User updated successfully!');
+            fetchUsers();
+            setUpdateUser(null);
+            clearUpdateForm();
+        } catch (error) {
+            console.error('Error updating user:', error);
+        }
+    };
 
-  const handleDeleteUser = async (userId) => {
-    try {
-      await db.collection('users').doc(userId).delete();
-      await auth.currentUser.delete();
+    const handleDeleteUser = async (userId) => {
+        try {
+            await db.collection('users').doc(userId).delete();
+            await auth.currentUser.delete();
 
-      alert('User deleted successfully!');
-      fetchUsers();
-    } catch (error) {
-      console.error('Error deleting user:', error);
-    }
-  };
+            alert('User deleted successfully!');
+            fetchUsers();
+        } catch (error) {
+            console.error('Error deleting user:', error);
+        }
+    };
 
-  const clearUpdateForm = () => {
-    setUpdateEmail('');
-    setUpdateFirstName('');
-    setUpdateLastName('');
-    setUpdateNumber('');
-    setUpdatePickup('');
-    setUpdateRoute('');
-  };
+    const clearUpdateForm = () => {
+        setUpdateEmail('');
+        setUpdateFirstName('');
+        setUpdateLastName('');
+        setUpdateNumber('');
+        setUpdatePickup('');
+        setUpdateRoute('');
+    };
 
-  return (
-    <div>
-      <h2>Update Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Number</th>
-            <th>Pickup</th>
-            <th>Route</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.email}</td>
-              <td>{user.firstName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.number}</td>
-              <td>{user.pickup}</td>
-              <td>{user.route}</td>
-              <td>
-                <button onClick={() => handleUpdateUser(user)}>Edit</button>
-                <button onClick={() => handleDeleteUser(user.id)}>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    return (
+        <div className="d-flex">
+            <SidebarTest activePage="updtUser"/>
+            <div className=" ps-4 pt-3">
+                <h2>Update Users</h2>
+                <table className="table  table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Number</th>
+                        <th>Pickup</th>
+                        <th>Route</th>
+                        <th>Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.email}</td>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                            <td>{user.number}</td>
+                            <td>{user.pickup}</td>
+                            <td>{user.route}</td>
+                            <td>
+                                <div className="btn-group">
+                                    <button type="button" className="btn btn-outline-dark dropdown-toggle"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                        Action
+                                    </button>
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <button className="dropdown-item "
+                                                    onClick={() => handleUpdateUser(user)}>Edit
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <hr className="dropdown-divider"/>
+                                        </li>
+                                        <li>
+                                            <button className="dropdown-item"
+                                                    onClick={() => handleDeleteUser(user.id)}>Delete
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
 
-      {updateUser && (
-        <div>
-          <h3>Update User</h3>
-          <form>
-            <label>
-              Email:
-              <input
-                type="email"
-                value={updateEmail}
-                onChange={(e) => setUpdateEmail(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              First Name:
-              <input
-                type="text"
-                value={updateFirstName}
-                onChange={(e) => setUpdateFirstName(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Last Name:
-              <input
-                type="text"
-                value={updateLastName}
-                onChange={(e) => setUpdateLastName(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Number:
-              <input
-                type="text"
-                value={updateNumber}
-                onChange={(e) => setUpdateNumber(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Pickup:
-              <input
-                type="text"
-                value={updatePickup}
-                onChange={(e) => setUpdatePickup(e.target.value)}
-              />
-            </label>
-            <br />
-            <label>
-              Route:
-              <input
-                type="text"
-                value={updateRoute}
-                onChange={(e) => setUpdateRoute(e.target.value)}
-              />
-            </label>
-            <br />
-            <button type="button" onClick={handleSaveUpdate}>
-              Save
-            </button>
-            <button type="button" onClick={clearUpdateForm}>
-              Cancel
-            </button>
-          </form>
+                {updateUser && (
+                    <div>
+                        <h3>Update User</h3>
+                        <form>
+                            <label>
+                                Email:
+                                <input
+                                    type="email"
+                                    value={updateEmail}
+                                    onChange={(e) => setUpdateEmail(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                First Name:
+                                <input
+                                    type="text"
+                                    value={updateFirstName}
+                                    onChange={(e) => setUpdateFirstName(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Last Name:
+                                <input
+                                    type="text"
+                                    value={updateLastName}
+                                    onChange={(e) => setUpdateLastName(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Number:
+                                <input
+                                    type="text"
+                                    value={updateNumber}
+                                    onChange={(e) => setUpdateNumber(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Pickup:
+                                <input
+                                    type="text"
+                                    value={updatePickup}
+                                    onChange={(e) => setUpdatePickup(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <label>
+                                Route:
+                                <input
+                                    type="text"
+                                    value={updateRoute}
+                                    onChange={(e) => setUpdateRoute(e.target.value)}
+                                />
+                            </label>
+                            <br/>
+                            <button type="button" onClick={handleSaveUpdate}>
+                                Save
+                            </button>
+                            <button type="button" onClick={clearUpdateForm}>
+                                Cancel
+                            </button>
+                        </form>
+                    </div>
+                )}
+            </div>
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default UpdtUser;
