4847f4c2877faa813e966368131bd6da436c15b0
Sas4aZ <paudelsashwat16@gmail.com>
2023-06-03 17:37:44 +0545

    admin icon fix

--- src/components/sidebar/SidebarTest.js
+++ src/components/sidebar/SidebarTest.js
@@ -96,14 +96,17 @@ const Sidebar = ({activePage}) => {
                     <div className="dropdown">
                         <a href="" className="d-flex align-items-center text-dark text-decoration-none dropdown-toggle"
                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="" width="32" height="32"
-                                 className="rounded-circle me-2"/>
-                            <strong>mdo</strong>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                                 className="rounded-circle me-2" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fill-rule="evenodd"
+                                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                            </svg>
+                            <strong>Admin</strong>
                         </a>
                         <ul className="dropdown-menu text-small shadow">
-                            <li><a className="dropdown-item" href="#">New project...</a></li>
-                            <li><a className="dropdown-item" href="#">Settings</a></li>
-                            <li><a className="dropdown-item" href="#">Profile</a></li>
+
+                            <li><a className="dropdown-item" href="#">In progress</a></li>
                             <li>
                                 <hr className="dropdown-divider"/>
                             </li>
