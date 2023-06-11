df7078398543b373161ee576dab8c278c35c725d
Sas4aZ <paudelsashwat16@gmail.com>
2023-06-11 14:27:57 +0545

    Home page design added

--- src/components/nav/HeadHome.js
+++ src/components/nav/HeadHome.js
@@ -3,25 +3,31 @@ import React from "react";
 function HeadHome() {
     return (
         <>
-        <div className="container-fluid  " id="custom_nav">
-            <div className="container">
-                <header className="d-flex flex-wrap justify-content-center py-3 mb-4  ">
-                    <a href="/"
-                       className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-black link-body-emphasis text-decoration-none">
-                        <img className="bi me-2" width="40" height="32" role="img"
-                             src="https://picsum.photos/200/300" alt=""/>
-                        <span className="fs-3">Sunischit</span>
-                    </a>
-                    <ul className="nav nav-pills">
-                        <li className="nav-item"><a href="/login" aria-current="page">
-                            <button className="btn btn-outline-primary">Login</button>
-                        </a>
-                        </li>
+            <div className="container ">
+                
+                <header
+                    className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 ">
+                    <div className="col-md-3 mb-2 mb-md-0">
+
+                    </div>
+
+                    <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+
+                        <img src="./sifalSchool.png" alt="" height="80" width="270"/>
                     </ul>
+
+
+                    <div className="col-md-3 text-end">
+                        <a href="/login">
+                            <button type="button" className="btn btn-outline-primary me-2">Login</button>
+                        </a>
+                    </div>
                 </header>
             </div>
-        </div>
+
+
         </>
     )
 }
+
 export default HeadHome;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -8,13 +8,42 @@ function Home() {
     return (
         <>
             <HeadHome/>
-            <div className="container">
-                <div className="titleContainer">
-                    <h1 className="title">The Sunischit App</h1>
-                    <p className="titleP">Assuring your child's safety</p>
+            <div className="container-fluid">
+                <div className="row ms-3">
+                    <div className="col">
+                        <img src="./login.png" alt="" width="217.5" height="476.25"/>
+                    </div>
+                    <div className="col-md-7 ">
+                        <div className="mt-5 pt-4">
+                            <div className=" d-flex align-items-center justify-content-center ">
+                                <h1 className='text big'>The <span className="coloured-orange">Sunischit </span> App
+                                </h1>
+                            </div>
+                            <div className=" d-flex align-items-center justify-content-center ">
+                                <p className='text fs-4'>Assuring your child's safety </p>
+                            </div>
+                            <div className=" d-flex align-items-center justify-content-center">
+                                <img src="./deerwalkBus.png" alt=""/>
+                            </div>
+                        </div>
+                        <div className=" mt-5 d-flex align-items-center justify-content-center ">
+                            <p className='text fs-6'>Developers: Aarnov Adhikari(Backend), Arpan Acharya (Full
+                                Stack), Sashwat Paudel (Frontend) </p>
+                        </div>
+                        <div className=" mt-3 d-flex align-items-center justify-content-center ">
+                            <p className='text  fs-6'><a className=' text-black'
+                                                         href="mailto:sunischitapp@gmail.com"> sunischitapp@gmail.com</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <img src="./map.png" width="217.5" height="476.25" alt=""/>
+                    </div>
                 </div>
             </div>
         </>
+
 
     );
 }
